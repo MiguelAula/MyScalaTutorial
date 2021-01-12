c501591b6e9480ccc008dad4ecb200e123dd28e6
Driond <maulacasellas@hotmail.com>
2021-01-12 10:20:15 +0100

    removed chapter 6 for now

--- src/main/scala/exercises/7_Collections_advanced.scala
+++ src/main/scala/exercises/7_Collections_advanced.scala
@@ -26,6 +26,41 @@ object CollectionsAdvancedExercises {
    */
   def reduceFL[A](l: List[A])(f: (A,A) => A): A = ???
 
+
+  /**
+   * _____________________________________________________
+   *  Extras for practice:
+   * _____________________________________________________
+   */
+
+  /**
+   * Imagine we have a matrix that stores the amount of cm³ of water that hold certain landmarks in Girona.
+   */
+  val matrix: List[List[Double]] =
+    List(
+      List(0.5,3,5.2),
+      List(1.2,6.2,0.1),
+      List(9.2,2.6,3.9)
+    )
+
+  /**
+   * It just rained 3 cm³ in all of Girona, which means we need a mean to update our matrix. Let's define a function that does this:
+   */
+  def updateMatrix(matrix: List[List[Double]])(op: Double => Double): List[List[Double]] = matrix.map(_.map(op(_)))
+
+
+  /**
+   * We need to be able to pour all of the water into any slot of the matrix to get it ready for collection
+   */
+  def pourTo(matrix: List[List[Double]], slot: (Int,Int)): List[List[Double]] = {
+    val acc = matrix.foldLeft(0.0)((n,l) => l.sum + n)
+    matrix.zipWithIndex.map { case (row, i) =>
+      row.zipWithIndex.map { case (_, j) =>
+        if (slot != (i, j)) 0
+        else acc
+      }
+    }
+  }
 }
 
 
--- src/main/scala/solutions/7_Collections_advanced.scala
+++ src/main/scala/solutions/7_Collections_advanced.scala
@@ -38,6 +38,42 @@ object CollectionsAdvancedExercises {
   def reduceFL[A](l: List[A])(f: (A,A) => A): A = l.foldLeft(???)(f)
   /* It cannot be done! We need to know which would be the initial value for the type A (a zero value) */
 
+
+  /**
+   * _____________________________________________________
+   *  Optional extras:
+   * _____________________________________________________
+   */
+
+  /**
+   *  6) Imagine we have a matrix that stores the amount of cm³ of water that hold certain landmarks in Girona.
+   */
+  val matrix: List[List[Double]] =
+    List(
+      List(0.5,3,5.2),
+      List(1.2,6.2,0.1),
+      List(9.2,2.6,3.9)
+    )
+
+  /**
+   *  7) It just rained 3 cm³ in all of Girona, which means we need a mean to update our matrix. Let's define a function that does this:
+   */
+  def updateMatrix(matrix: List[List[Double]])(op: Double => Double): List[List[Double]] = matrix.map(_.map(op(_)))
+
+
+  /**
+   *  8) We need to be able to pour all of the water into any slot of the matrix to get it ready for collection
+   */
+  def pourTo(matrix: List[List[Double]], slot: (Int,Int)): List[List[Double]] = {
+    val acc = matrix.foldLeft(0.0)((n,l) => l.sum + n)
+    matrix.zipWithIndex.map { case (row, i) =>
+      row.zipWithIndex.map { case (_, j) =>
+        if (slot != (i, j)) 0
+        else acc
+      }
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     println(
       foldLeft(List(1,2,3),0)(_ + _*2)
@@ -45,6 +81,16 @@ object CollectionsAdvancedExercises {
 
     println(
       foldRight(List(1,2,3),0)(_*2 + _)
+    )
+
+    /* ----------------------------- */
+
+    println(
+      updateMatrix(matrix)(_ + 1)
+    )
+
+    println(
+      pourTo(matrix,(0,1))
     )
   }
 
